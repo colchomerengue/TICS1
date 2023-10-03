5b194d2904fee2e9453a47b045e15f27e8d04df1
colchomerengue <matgarlar@iesxulianmagarinos.org>
2023-10-03 12:16:29 +0200

    k

--- boletins/Boletin2.py
+++ boletins/Boletin2.py
@@ -41,3 +41,26 @@ if c%2==0:
 else:
     print(c,"es impar")
 
+#Ej5
+
+año=int(input("¿Cúantos años tienes?"))
+if año>=16 :
+    print("si que tributas")
+else print("no tributas")
+
+
+#Ej6
+nombre=input("¿Cómo te llamas?").lower()
+sexo=input("¿eres mujer? s/n").lower()
+if (nombre<"m" and sexo=="s") or (nombre>"n" and sexo=="n"):
+    print("EStas en el grupo A")
+elif ( nombre>"n" and sexo=="s") or (nombre <"m" and sexo=="n"):
+    print("EStas en el grupo b")
+
+#Ej7
+
+
+
+
+
+
