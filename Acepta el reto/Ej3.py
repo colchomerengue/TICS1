37014dc792f5af86960a405de37a232557b6b0b0
colchomerengue <matgarlar@iesxulianmagarinos.org>
2023-11-07 11:56:14 +0100

    mateo peor>>>mateo mejor

--- Acepta el reto/Ej3.py
+++ Acepta el reto/Ej3.py
@@ -1,9 +1,12 @@
 dia=int(input("Que día es"))
 mes=int(input("Y el mes"))
 anho=[31 ,28 ,31 ,30 ,31 ,30 ,31 , 31, 30, 31, 30, 31]
-tem_anho=[mes:12]
-res=anho [mes-1]-dia
+tem_anho=anho[mes:12]
+res=anho[mes-1]-dia
+
 for i in tem_anho:
     res=res+i
+
+print(res)
 
 
