56cf6af0c0ed954b0cb9f9860b5b83ae8ba3e05b
colchomerengue <matgarlar@iesxulianmagarinos.org>
2024-01-26 12:05:51 +0100

    spca

--- Juegos/AER145.py
+++ Juegos/AER145.py
@@ -1,24 +1,17 @@
-import datetime 
+import datetime
+myhora=input("Por favor introduce una hora(hh:mm)")
+myhora=datetime.datetime.strptime(myhora, "%H:%M").time()
+hora_espejo=myhora
+
+if myhora.hour!=12:
+    hora_espejo=hora_espejo.replace(hour=12-myhora.hour)
+if myhora.minute!=0:
+    hora_espejo=hora_espejo.replace(minute=60-myhora.minute)
 
 
-#ejemplo de fecha completa
-fecha=datetime.date.today()
-print(fecha)
-#ejemplo    
-hora=datetime.time(10,30)
-print(hora.hour)
-print(hora.minute)
-
-
-#print(type(hora)
-hora=hora.replace(minute=28)
-#print
-
-date_time_str= "08:20"
-date_time_object=datetime.datetime.strptime(date_time_str, "%H:%M")
-print(date_time_str)
-print(date_time_object)
-mihora=datetime.datetime.strptime(mihora, "%H:%M").time()
-hora_esp=mihora
-hora_esp=hora_esp.replace(60-(minute=28))
+if myhora.minute!=0 and myhora.minute<30:
+    hora_espejo=hora_espejo.replace(hour=hora_espejo.hour-1)
+    print(hora_espejo)    
+else:
+    print(hora_espejo)
 
--- REPASO/reloj_espejo.py
+++ REPASO/reloj_espejo.py
@@ -2,4 +2,4 @@ import datetime
 mihora=input("Introduce la hora en formato HH:MM ")
 mihora=datetime.datetime.strptime(mihora, "%H:%M").time()
 hora_esp=mihora
-hora_esp=hora_esp.replace(60-(minute=28))
+hora_esp=hora_esp.replace(hora_esp.minute)
